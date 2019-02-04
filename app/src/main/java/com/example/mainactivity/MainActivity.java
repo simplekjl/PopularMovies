9fd9990d78334821a1a2e42137253fa96bad29ba
Jose L Crisostomo <jl.cs606@gmail.com>
2019-02-03 16:16:54 -0800

    Project setup
    Android project setup for the course, includes just configuration level

---
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+ * Develop by Jose L Crisostomo S. on 2/3/19 4:08 PM
+ * Last modified 2/3/19 4:08 PM.
+ * Copyright (c) 2019. All rights reserved.
+ *
+ *
+ */
+
+package com.example.mainactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
