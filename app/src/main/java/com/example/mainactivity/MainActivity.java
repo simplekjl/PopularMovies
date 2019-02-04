089aa541c45de7f5c17474c6ea0cac1c8b4104b2
Jose L Crisostomo <jl.cs606@gmail.com>
2019-02-04 01:31:06 -0800

    MoviesAdapter
    *Movies adapter
    *adapter Layout
    *strings
    *dependencies

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -10,44 +10,149 @@ package com.example.mainactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.mainactivity.adapters.MoviesAdapter;
+import com.example.mainactivity.network.MoviesDBClient;
+import com.example.mainactivity.network.MoviesDBService;
+import com.example.mainactivity.network.model.MoviesResponse;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar mProgressBar;
     private RecyclerView mRecyclerView;
+    private MoviesAdapter mMoviesAdapter;
+    private TextView mErrorMessage;
+    private static String TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+        getMostPopularMovies();
+    }
+
+    private void bindViews() {
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_movies);
+        mErrorMessage = (TextView) findViewById(R.id.error_message);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()){
-
+        switch (item.getItemId()) {
             case R.id.action_most_popular:
-                //TODO(1) make a service call to sort the movies by most popular
+                getMostPopularMovies();
                 return true;
             case R.id.action_highest_rated:
-                //TODO(2) make a service call to sort the movies by highest rated
+                getTopRatedMovies();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void getTopRatedMovies() {
+        MoviesDBService service = MoviesDBClient.getInstance().create(MoviesDBService.class);
+        Call<MoviesResponse> result = service.getHihestRatedMovies(getString(R.string.api_key));
+        showloader();
+        result.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        MoviesResponse moviesResponse = response.body();
+                        if (moviesResponse.getMoviesList() != null) {
+                            mMoviesAdapter = new MoviesAdapter(moviesResponse.getMoviesList());
+                            showResults();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                showErrorMessage();
+                Log.e(TAG, "onFailure: "+ t.getMessage() );
+            }
+        });
+    }
+
+    private void getMostPopularMovies() {
+        MoviesDBService service = MoviesDBClient.getInstance().create(MoviesDBService.class);
+        Call<MoviesResponse> result = service.getMostPopularMovies(getString(R.string.api_key));
+        showloader();
+        result.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                if(response.isSuccessful()){
+                if (response.body() != null) {
+                    MoviesResponse moviesResponse = response.body();
+                    if (moviesResponse.getMoviesList() != null) {
+                        mMoviesAdapter = new MoviesAdapter(moviesResponse.getMoviesList());
+                        showResults();
+                    }}
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                showErrorMessage();
+                Log.e(TAG, "onFailure: "+t.getMessage());
+            }
+        });
+    }
+
+    void showloader() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.INVISIBLE);
+    }
+
+    //region ShowResults
+    void showResults() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mErrorMessage.setVisibility(View.INVISIBLE);
+        //setup recyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mMoviesAdapter);
 
     }
+    //endRegion showresults
+
+    //region Error Message
+    void showErrorMessage() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.VISIBLE);
+    }
+    //endRegion error message
 }
---
+++ app/src/main/java/com/example/mainactivity/adapters/MoviesAdapter.java
@@ -0,0 +1,108 @@
+/*
+ * Develop by Jose L Crisostomo S. on 2/3/19 6:53 PM
+ * Last modified 2/3/19 6:53 PM.
+ * Copyright (c) 2019. All rights reserved.
+ *
+ *
+ */
+
+package com.example.mainactivity.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.mainactivity.R;
+import com.example.mainactivity.network.MoviesDBClient;
+import com.example.mainactivity.network.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
+
+    private List<Movie> mMoviesDataSet;
+
+    public MoviesAdapter(List<Movie> data) {
+        mMoviesDataSet = data;
+    }
+
+    @NonNull
+    @Override
+    public MoviesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int itemLayout = R.layout.movie_item;
+        boolean hastoBeAttachedRightAway = false;
+        View view = LayoutInflater.from(context)
+                .inflate(itemLayout, viewGroup, hastoBeAttachedRightAway);
+
+        MoviesAdapterViewHolder viewHolder = new MoviesAdapterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapterViewHolder moviesAdapterViewHolder, int i) {
+
+        moviesAdapterViewHolder.setItem(mMoviesDataSet.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMoviesDataSet.size();
+    }
+
+    class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitle;
+        private TextView mReleaseDate;
+        private ImageView mImageView;
+        private RatingBar mRatingBar;
+        private TextView mRating;
+        private TextView mSynopsis;
+
+
+        MoviesAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bindViews(itemView);
+        }
+
+        void setItem(Movie movie) {
+            mTitle.setText(movie.getTitle());
+            mReleaseDate.setText(movie.getReleaseDate());
+            mRatingBar.setRating(movie.getVotesAvg());
+            mRating.setText(String.valueOf(movie.getVotesAvg()));
+            StringBuilder sb = new StringBuilder(150);
+            if (movie.getPosterPath() != null) {
+                sb = new StringBuilder(150);
+                sb.append(MoviesDBClient.IMAGE_BASE_URL);
+                sb.append(movie.getPosterPath());
+            } else {
+                sb.append(MoviesDBClient.IMAGE_BASE_URL);
+            }
+
+            Picasso.get()
+                    .load(sb.toString())
+                    .placeholder(R.drawable.thumbnail)
+                    .error(R.drawable.thumbnail)
+                    .into(mImageView);
+            mSynopsis.setText(movie.getOverview());
+        }
+
+        private void bindViews(View view) {
+            mTitle = view.findViewById(R.id.tv_title);
+            mReleaseDate = view.findViewById(R.id.tv_release_date);
+            mImageView = view.findViewById(R.id.iv_poster);
+            mRatingBar = view.findViewById(R.id.ratingBar);
+            mRating = view.findViewById(R.id.tv_rating);
+            mSynopsis = view.findViewById(R.id.tv_synopsis);
+
+        }
+    }
+}
